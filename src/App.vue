d14ea82a63b89d1f31bc30a47e05631931de8be4
左风 <595563214@qq.com>
2023-02-16 18:24:10 +0800

    feat：提交所有代码

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template><RouterView /></template>
+
+<style>
+@media screen and (min-width: 760px) and (max-width: 5000px) {
+  body {
+    background-color: #f7f8fa;
+  }
+}
+</style>
